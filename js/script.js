9eb88ba07d643babb0ad1d188515509ddda53e0d
qsdcv301 <qsdcv301@naver.com>
2024-08-26 17:53:41 +0900

    메인화면 레이아웃 디자인

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $(".photos-gallery").pycsLayout({
         pictureContainer:".picture",
         gutter: 5,
-        idealHeight: 150
+        idealHeight: 120
     });
 });
